f844ad55da3f22519bf06a3a115b36a12df861cf
fdm-julianlim <julian.lim@fdmgroup.com>
2026-09-04 14:20:37 +0800

    Project initialisation

---
+++ insurance-claim/src/main/java/com/fdmgroup/insurance_claim/InsuranceClaimApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.insurance_claim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceClaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClaimApplication.class, args);
+	}
+
+}
---
+++ insurance-claim/src/test/java/com/fdmgroup/insurance_claim/InsuranceClaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.insurance_claim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceClaimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
